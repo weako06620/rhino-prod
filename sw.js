b2df06ecd53338f256125cff17a615de7378ffc0
weako06620 <weako06800@gmail.com>
2026-03-26 16:34:20 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+// Cache version : 1774524784
+const CACHE_NAME = 'prehistoria-v1774524784';
+const ASSETS = ['./', './index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./')))
+  );
+});
